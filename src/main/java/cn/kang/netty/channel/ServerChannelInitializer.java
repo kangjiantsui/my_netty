3d3e755d4c160777ff224f5e4ea1026a751455c1
kangjian <kangjiantsui@163.com>
2019-11-27 19:41:32 +0800

    把ServerFrameHandler捡回来了,同时处理http请求和websocket请求的handler有问题,没有返回给客户端

--- src/main/java/cn/kang/netty/channel/ServerChannelInitializer.java
+++ src/main/java/cn/kang/netty/channel/ServerChannelInitializer.java
@@ -1,7 +1,10 @@
 package cn.kang.netty.channel;
 
 import cn.kang.common.protocol.PersonMessage;
+import cn.kang.netty.handler.ServerFrameHandler;
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.MessageLite;
+import com.google.protobuf.MessageLiteOrBuilder;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
@@ -10,14 +13,21 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import io.netty.handler.codec.MessageToMessageEncoder;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.*;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.stream.ChunkedWriteHandler;
 
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static io.netty.buffer.Unpooled.wrappedBuffer;
 
 /**
  * 可以同时处理http和websocket请求的channel
@@ -40,101 +50,101 @@ public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> 
         pipeline.addLast(new ChunkedWriteHandler());
         pipeline.addLast(new LengthFieldBasedFrameDecoder(16777215, 0, 4, 0, 4));
         pipeline.addLast(new LengthFieldPrepender(4));
-//        pipeline.addLast(new NettyConnectServerHandler(new AtomicInteger(0)));
-        pipeline.addLast(new SimpleChannelInboundHandler() {
+//        pipeline.addLast(new SimpleChannelInboundHandler() {
+//
+//            @Override
+//            public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+//                super.channelRegistered(ctx);
+//                connectNum.addAndGet(1);
+//                System.out.println("连接接入,目前连接数:" + connectNum);
+//            }
+//
+//            @Override
+//            public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+//                super.channelUnregistered(ctx);
+//                connectNum.addAndGet(-1);
+//                System.out.println("连接断开,目前连接数:" + connectNum);
+//            }
+//
+//            @Override
+//            protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+//                if (msg instanceof FullHttpRequest) {
+//                    handleHttpRequest(ctx, (FullHttpRequest) msg);
+//                } else if (msg instanceof WebSocketFrame) {
+//                    handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+//                }
+//            }
+//
+//            // 处理HTTP的代码
+//            private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) throws UnsupportedEncodingException {
+//                // 如果是websocket请求就握手升级
+//                if ("/ws".equalsIgnoreCase(req.uri())) {
+//                    System.out.println("websocket 请求接入");
+//                    WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory("/ws", null, false);
+//                    handshaker = wsFactory.newHandshaker(req);
+//                    handshaker.handshake(ctx.channel(), req);
+//                }
+//            }
+//
+//            // 处理Websocket的代码
+//            private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) throws InvalidProtocolBufferException, InterruptedException {
+//                if (frame instanceof CloseWebSocketFrame) {
+//                    ctx.close().sync();
+//                } else if (frame instanceof PingWebSocketFrame) {
+//                    ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+//                } else {
+//                    ByteBuf buf = frame.content();
+//                    byte[] bytes = new byte[buf.readableBytes()];
+//                    buf.readBytes(bytes);
+//                    PersonMessage.Person person = PersonMessage.Person.parseFrom(bytes);
+//                    System.out.println(person.getId());
+//                    System.out.println(person.getName());
+//                    System.out.println("连接数:" + connectNum);
+//                    ctx.channel().write(1);
+//                }
+//            }
+//        });
 
+        // WebSocket数据压缩
+        pipeline.addLast(new WebSocketServerCompressionHandler());
+        // 协议包长度限制
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws", null, true));
+        //pipeline.addLast(new HttpHandler());
+        // 协议包解码
+        pipeline.addLast(new MessageToMessageDecoder<WebSocketFrame>() {
             @Override
-            public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
-                super.channelRegistered(ctx);
-                connectNum.addAndGet(1);
-                System.out.println("连接接入,目前连接数:" + connectNum);
+            protected void decode(ChannelHandlerContext ctx, WebSocketFrame frame, List<Object> objs) {
+                ByteBuf buf = frame.content();
+                objs.add(buf);
+                buf.retain();
             }
-
+        });
+        // 协议包编码
+        pipeline.addLast(new MessageToMessageEncoder<MessageLiteOrBuilder>() {
             @Override
-            public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
-                super.channelUnregistered(ctx);
-                connectNum.addAndGet(-1);
-                System.out.println("连接断开,目前连接数:" + connectNum);
-            }
-
-            @Override
-            protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-                if (msg instanceof FullHttpRequest) {
-                    handleHttpRequest(ctx, (FullHttpRequest) msg);
-                } else if (msg instanceof WebSocketFrame) {
-                    handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+            protected void encode(ChannelHandlerContext ctx, MessageLiteOrBuilder msg, List<Object> out) {
+                ByteBuf result = null;
+                if (msg instanceof MessageLite) {
+                    result = wrappedBuffer(((MessageLite) msg).toByteArray());
                 }
-            }
-
-            // 处理HTTP的代码
-            private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) throws UnsupportedEncodingException {
-                // 如果是websocket请求就握手升级
-                if ("/ws".equalsIgnoreCase(req.uri())) {
-                    System.out.println("websocket 请求接入");
-                    WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory("/ws", null, false);
-                    handshaker = wsFactory.newHandshaker(req);
-                    handshaker.handshake(ctx.channel(), req);
+                if (msg instanceof MessageLite.Builder) {
+                    result = wrappedBuffer(((MessageLite.Builder) msg).build().toByteArray());
                 }
-            }
 
-            // 处理Websocket的代码
-            private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) throws InvalidProtocolBufferException, InterruptedException {
-                if (frame instanceof CloseWebSocketFrame) {
-                    ctx.close().sync();
-                } else if (frame instanceof PingWebSocketFrame) {
-                    ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
-                } else {
-                    ByteBuf buf = frame.content();
-                    byte[] bytes = new byte[buf.readableBytes()];
-                    buf.readBytes(bytes);
-                    PersonMessage.Person person = PersonMessage.Person.parseFrom(bytes);
-                    System.out.println(person.getId());
-                    System.out.println(person.getName());
-                    System.out.println("连接数:"+connectNum);
-                }
+                // ==== 上面代码片段是拷贝自TCP ProtobufEncoder 源码 ====
+                // 然后下面再转成websocket二进制流，因为客户端不能直接解析protobuf编码生成的
+
+                assert result != null;
+                WebSocketFrame frame = new BinaryWebSocketFrame(result);
+                out.add(frame);
             }
         });
 
-//        // WebSocket数据压缩
-//        pipeline.addLast(new WebSocketServerCompressionHandler());
-//        // 协议包长度限制
-//        pipeline.addLast(new WebSocketServerProtocolHandler("/ws", null, true));
-//        //pipeline.addLast(new HttpHandler());
-//        // 协议包解码
-//        pipeline.addLast(new MessageToMessageDecoder<WebSocketFrame>() {
-//            @Override
-//            protected void decode(ChannelHandlerContext ctx, WebSocketFrame frame, List<Object> objs) {
-//                ByteBuf buf = frame.content();
-//                objs.add(buf);
-//                buf.retain();
-//            }
-//        });
-//        // 协议包编码
-//        pipeline.addLast(new MessageToMessageEncoder<MessageLiteOrBuilder>() {
-//            @Override
-//            protected void encode(ChannelHandlerContext ctx, MessageLiteOrBuilder msg, List<Object> out) {
-//                ByteBuf result = null;
-//                if (msg instanceof MessageLite) {
-//                    result = wrappedBuffer(((MessageLite) msg).toByteArray());
-//                }
-//                if (msg instanceof MessageLite.Builder) {
-//                    result = wrappedBuffer(((MessageLite.Builder) msg).build().toByteArray());
-//                }
-//
-//                // ==== 上面代码片段是拷贝自TCP ProtobufEncoder 源码 ====
-//                // 然后下面再转成websocket二进制流，因为客户端不能直接解析protobuf编码生成的
-//
-//                assert result != null;
-//                WebSocketFrame frame = new BinaryWebSocketFrame(result);
-//                out.add(frame);
-//            }
-//        });
-//
-//        // 协议包解码时指定Protobuf字节数实例化为CommonProtocol类型
-//        pipeline.addLast(new ProtobufDecoder(PersonMessage.Person.getDefaultInstance()));
-//
-//        // websocket定义了传递数据的6中frame类型
-//        pipeline.addLast(new ServerFrameHandler());
+        // 协议包解码时指定Protobuf字节数实例化为CommonProtocol类型
+        pipeline.addLast(new ProtobufDecoder(PersonMessage.Person.getDefaultInstance()));
+
+        // websocket定义了传递数据的6中frame类型
+        pipeline.addLast(new ServerFrameHandler());
 
 
     }
